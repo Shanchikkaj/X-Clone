3679a68395905e16e2dc5767c28dec0316479196
Shanchikkaj <shanchikkajeganathan@gmail.com>
2025-05-18 21:09:29 +0530

    Update modified and new files

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,18 @@
-export const signup = (req,res)=>{
-    res.send("Signup");
+export const signup = async(req,res)=>{
+    try{
+       const {username, fullName, email,password} =req.body;
+       
+    } catch(error){
+        console.log(`Error in singup controller: ${error }`)
+        res.status(500).json({error: "Internal Server Error"})
+    }
+ 
+}
+
+export const login= (req,res)=>{
+    res.send("login controller")
+}
+
+export const logout = (req,res)=>{
+    res.send("logout controller")
 }
--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,18 @@ import express from "express"
 import dotenv from "dotenv"
 
 import authRoute from "./routes/auth.route.js"
+import connectDB from "./db/connectDB.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 app.use("/api/auth", authRoute);
+
 
 app.listen(PORT,()=>{
     console.log(`Server is Running on ${PORT}`);
+    connectDB();
 })
