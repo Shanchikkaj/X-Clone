93264a8d34f98e036ca7c1d2b5799ba4883371b2
Shanchikkaj <shanchikkajeganathan@gmail.com>
2025-05-28 14:54:57 +0530

    remove sceret key and update generateToken file

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
 
 export const signup = async(req,res)=>{
     
@@ -34,6 +35,7 @@ export const signup = async(req,res)=>{
     })
 
     if(newUser){
+        generateToken(newUser._id, res)
         await newUser.save();
         res.status(200).json({
             _id : newUser.username,
