59b2b21590525a2d4fb09a13abd267ff9dd4922f
Shanchikkaj <shanchikkajeganathan@gmail.com>
2025-05-30 03:24:10 +0530

    Updated auth controller safely

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
 
 import authRoute from "./routes/auth.route.js"
+import userRoute from "./routes/user.route.js"
 import connectDB from "./db/connectDB.js";
 
 dotenv.config();
@@ -9,9 +11,14 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+//use for get cookie
+app.use(cookieParser());
+
+
 
 app.use("/api/auth", authRoute);
-
+// route for user
+app.use("/api/users", userRoute);
 
 app.listen(PORT,()=>{
     console.log(`Server is Running on ${PORT}`);
